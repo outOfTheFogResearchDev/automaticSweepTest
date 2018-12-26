66497d1a95a1aa892de9982c58bcc45cd9725069
Max <maxl.berkowitz@gmail.com>
2018-12-26 11:29:18 -0800

    step 2

--- server/app/api/index.js
+++ server/app/api/index.js
@@ -3,14 +3,27 @@ const { writeCsv, readCsv } = require('../util/utils');
 
 const api = Router();
 
-api.post('/sweep', async (req, res) => {
-  const { channel, data, unit, type } = req.body;
-  try {
-    await writeCsv(channel, data, unit, type);
-    res.sendStatus(201);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
+api
+  .route('/sweep')
+  .post(async (req, res) => {
+    const { channel, sweep, unit, type } = req.body;
+    try {
+      await writeCsv(channel, sweep, unit, type);
+      res.sendStatus(201);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  })
+  .get(async (req, res) => {
+    const { channel, unit } = req.query;
+    try {
+      const low = await readCsv(channel, unit, 'low');
+      const high = await readCsv(channel, unit, 'high');
+      const sweep = low.concat(high);
+      res.status(200).send({ sweep });
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  });
 
 module.exports = api;
