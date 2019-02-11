7338ab07c6ca65256ac613881cc4031da164e38d
Max <maxl.berkowitz@gmail.com>
2019-02-11 13:32:31 -0800

    temp after sweep set up

--- startAutomaticSweepTest.bat
+++ startAutomaticSweepTest.bat
@@ -1,7 +1,7 @@
 @echo off
 goto again
 :again
-    set /p answer=Is the Signal Analyzer pluged into the computer (or are you just trying to print data)? (Y/N)
+    set /p answer=Are the Signal Analyzer and the XF05 Communication Box pluged into the computer (or are you just trying to print data)? (Y/N)
     if /i "%answer:~,1%" EQU "Y" goto run
     if /i "%answer:~,1%" EQU "N" exit /b
     echo Please type Y for Yes or N for No
