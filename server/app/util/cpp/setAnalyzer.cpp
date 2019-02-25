3763c3e54ce982a3ef4d60d0c6e3e5350c6b9c1a
Max <maxl.berkowitz@gmail.com>
2019-02-25 15:17:13 -0800

    sweep acuracy upgrade

--- server/app/util/cpp/setAnalyzer.cpp
+++ server/app/util/cpp/setAnalyzer.cpp
@@ -29,8 +29,6 @@ void setAnalyzer(const FunctionCallbackInfo<Value> &args)
     // Set band center and marker
     viPrintf(viMXA, "FREQ:CENTER %f GHz\n", frequency);
     viPrintf(viMXA, "FREQ:SPAN 2 MHz\n");
-    viPrintf(viMXA, "CALC:MARK1:MODE OFF\n");
-    viPrintf(viMXA, "CALC:MARK1:SET:CENT\n");
 
     viClose(viMXA);     // closes session
     viClose(defaultRM); // closes default session
